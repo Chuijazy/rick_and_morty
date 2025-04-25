7083cc965784132b8672d0dd1124f84e9e69d1f9
Chuijazy <chuijazy.2004@gmail.com>
2025-04-25 16:19:48 +0600

    characters info screen

--- rick_and_morty/lib/main.dart
+++ rick_and_morty/lib/main.dart
@@ -1,25 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:rick_and_morty/modules/characters/data/model/repository/characters_list_repository.dart';
+import 'package:rick_and_morty/modules/characters/presentation/bloc/characters_bloc.dart';
+import 'package:rick_and_morty/modules/characters/presentation/bloc/characters_event.dart';
 import 'package:rick_and_morty/modules/characters/presentation/pages/home_screen.dart';
 
 void main() {
-  runApp(const RickAndMorty());
+  runApp(const MyApp());
 }
 
-class RickAndMorty extends StatelessWidget {
-  const RickAndMorty({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        scaffoldBackgroundColor: const Color(0xff0B1E2D),
-        bottomNavigationBarTheme: const BottomNavigationBarThemeData(
-          backgroundColor: Color(0xff152A3A),
-          selectedItemColor: Colors.white,
-          unselectedItemColor: Colors.white54,
-        ),
+    return BlocProvider(
+      create:
+          (_) =>
+              CharactersBloc(CharactersListRepository())..add(LoadCharacters()),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Rick and Morty',
+        theme: ThemeData.dark(),
+        home: const HomeScreen(),
       ),
-      home: const HomeScreen(),
     );
   }
 }
--- rick_and_morty/lib/modules/characters/presentation/pages/home_screen.dart
+++ rick_and_morty/lib/modules/characters/presentation/pages/home_screen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:rick_and_morty/modules/characters/data/model/repository/characters_list_repository.dart';
 import 'package:rick_and_morty/modules/characters/domain/entity/characters_entity.dart';
+import 'package:rick_and_morty/modules/characters/presentation/pages/screen_details/characters_list_view.dart';
 import 'package:rick_and_morty/modules/characters/presentation/pages/screen_details/custom_bottom_navigator_bar.dart';
+import 'package:rick_and_morty/modules/characters/presentation/pages/screen_details/search_appbar.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -41,86 +43,11 @@ class _HomeScreenState extends State<HomeScreen> {
 
     return Scaffold(
       backgroundColor: const Color(0xff0B1E2D),
-      appBar: AppBar(
-        backgroundColor: const Color(0xff0B1E2D),
-        title: TextField(
-          controller: _searchController,
-          style: const TextStyle(color: Colors.white),
-          decoration: const InputDecoration(
-            hintText: 'Search character...',
-            hintStyle: TextStyle(color: Colors.white54),
-            border: InputBorder.none,
-            icon: Icon(Icons.search, color: Colors.white),
-          ),
-        ),
-      ),
+      appBar: SearchAppBar(controller: _searchController),
       body: SafeArea(
-        child: ListView.builder(
-          itemCount: filteredCharacters.length + 1,
-          itemBuilder: (context, index) {
-            if (index == 0) {
-              return const Padding(
-                padding: EdgeInsets.symmetric(vertical: 16.0, horizontal: 16.0),
-                child: Text(
-                  'Total Characters: 20',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 10,
-                    fontWeight: FontWeight.w500,
-                  ),
-                ),
-              );
-            }
-
-            final character = filteredCharacters[index - 1];
-            final statusColor =
-                character.status.toLowerCase() == 'unknown'
-                    ? Colors.grey
-                    : character.status.toLowerCase() == 'alive'
-                    ? Colors.green
-                    : Colors.red;
-
-            return ListTile(
-              contentPadding: const EdgeInsets.symmetric(
-                horizontal: 10,
-                vertical: 6,
-              ),
-              leading: CircleAvatar(
-                radius: 35,
-                backgroundImage: NetworkImage(character.image),
-                backgroundColor: Colors.transparent,
-              ),
-              title: Text(
-                character.status,
-                style: TextStyle(
-                  color: statusColor,
-                  fontSize: 12,
-                  fontWeight: FontWeight.w500,
-                ),
-              ),
-              subtitle: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    character.name,
-                    style: const TextStyle(
-                      color: Colors.white,
-                      fontSize: 18,
-                      fontWeight: FontWeight.w600,
-                    ),
-                  ),
-                  Text(
-                    '${character.species}, ${character.gender}',
-                    style: const TextStyle(
-                      color: Colors.white70,
-                      fontSize: 14,
-                      fontWeight: FontWeight.w400,
-                    ),
-                  ),
-                ],
-              ),
-            );
-          },
+        child: CharactersListView(
+          characters: filteredCharacters,
+          searchQuery: _searchCharacter,
         ),
       ),
       bottomNavigationBar: CustomBottomNavigatorBar(),
